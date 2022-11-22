4c7726cb8dbcfcc28926b3edb01276c82e895f35
hammadhassan <hammad1997602@gmail.com>
2022-11-22 23:52:19 +0500

    routing & navigation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./routes/home/home.compent";
+import Navbar from "./routes/Navbar/Navbar.component";
 
+
+const Shop = () => {
+  return (
+    <div>
+      <h1>I am Shop Page</h1>
+    </div>
+  )
+}
 const App = () => {
-    return (
-      <Routes>
-        <Route path="/" element={<Home></Home>}></Route>
-      </Routes>
+  return (
+    <Routes>
+      <Route path="/" element={<Navbar/>}>
+        <Route index element={<Home />}></Route>
+        <Route path='shop' element={<Shop />}></Route>
+      </Route>
+    </Routes>
   )
 };
 
